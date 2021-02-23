5e5c4fe54d2179d5112bb108052cffcd9fe9926f
diego-Dell <registros.d@gmail.com>
2021-02-23 17:00:12 -0300

    Repositorio Clonado

--- eje1.js
+++ eje1.js
@@ -8,3 +8,4 @@ console.log("Variable 1: "+i);
 console.log("Variable 2: "+j);
 console.log("Suma->"+suma);
 console.log("Editado: 22-02-2021->19:00");
+console.log("Repositorio clonado: 23-02-2021->19:00");
